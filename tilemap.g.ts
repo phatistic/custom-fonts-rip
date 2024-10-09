b5eeda04612a7eb8d3c9ca37e22eef7b67c84448
phatistic <150102276+phatistic@users.noreply.github.com>
2024-10-09 09:12:23 +0700

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16_1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16_1":return transparency16_1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
